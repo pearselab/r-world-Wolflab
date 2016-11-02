518c3de8a22d2f7aa049d43bd32e8de54858893c
Paul Wolf <Paul.wolf@usu.edu>
2016-11-02 11:54:06 -0600

    here we go

--- wolf_terrain.R
+++ wolf_terrain.R
@@ -10,10 +10,14 @@
 #   4: DIAMOND-STEP(matrix)
 #     4.1: find mid-point (divide by two - check)
 #     4.2: make that mid-point an average of corners - add noise (jitter?)
+#     4.3: feed that value back to main landscape matrix
+#     4.4: Do this for ALL sub-squared (important)
 #   5: SQUARE-STEP(matrix)
 #     5.1: find mid points of all edges
-#     5.2: make that mid-point an average of adjacent corners and center - add noise (jitter?)
-#     5.3: *** tricky*** if this is an internal square, do I need to include CEDNTER from another
-#          square (hard to explain. See if Will understands what I mean)
+#     5.2: make that mid-point an average of adjacent corners and centers - add noise (jitter?)
+#     5.3: *** tricky*** if this is an internal square, do I need to include CENTER from another
+#          square (hard to explain. Must refer to full landscape matrix)
+#     5.4: feed that value back to main landscape matrix
+#     5.5: Do this for all sub-squares
 # 6: until Matrix filled with values
 # 7. Convert matrix to terrain using heat map, blue for below zero
