d3ecc8c1f21a0369220a9b6ae31c940aad60e424
Paul Wolf <Paul.wolf@usu.edu>
2016-11-07 19:36:44 -0700

    figuring out vectors and initiating them corretly

--- play.with.vector.arithmetic.R
+++ play.with.vector.arithmetic.R
@@ -1,10 +1,15 @@
 #vector arithmetic
-# Let's make sure I understand that "numeric" is used to INITIATE and empty vector of length
+# Let's make sure I understand that "numeric" is used to INITIATE and\ empty vector of length n
 # (single arguemnt required)
 # Whereas "c" means concantenate all arguments to be the actual values in the vector
 
 # This example show first how to fill a vector the long-winded loop method
-# Followed by the simpelr vector arithmetic approach
+# Followed by the simpler vector arithmetic approach
+# This seems to work if I want to do everything to every element of a vector
+# But cannot get approach to work if a) subsetting  B) conditionally apply function (will need an if
+# statement somewhere) or c) the new vector elements are a function of other elements. In this case
+# I either need a loop, or I must figure out how to do recursive function for vector, which does
+# not seem to work
 
 paul.sq.long.winded <- function(numb.elements){
   sq.vect <- numeric(numb.elements)
@@ -25,3 +30,5 @@ print(paul.square(15))
 print(paul.sq.long.winded(7))
 
 
+new.vect <- 1:10
+sum(new.vect)
--- wolf.plants.R
+++ wolf.plants.R
@@ -1,8 +1,15 @@
 setup.plants <- function(repro, survive, comp.mat, names=NULL){
+  # Note that I really like this dfensive programming stuff!
   if(is.null(names))
     names <- letters[seq_along(repro)]
   if(length(repro) != length(survive))
     stop("Reproduction and survival parameters needed for all species")
+  if(ncol(comp.mat) != length(survive))
+    stop("you dimwit, check your matrix size versus survive vector")
+  if(any(survive > 1.0) | (any(survive < 0)))
+    stop("Silly noodle: survive probabilities cannot be greater than 1.0 or less than zero")
+  if(any(repro > 1.0) | (any(repro < 0)))
+    stop("Silly noodle: repro probabilities cannot be greater than 1.0 or less than zero")
   #...some more tests...
   repro <- setNames(repro, names)
   survive <- setNames(survive, names)
@@ -15,9 +22,18 @@ setup.plants <- function(repro, survive, comp.mat, names=NULL){
 }
 
 repro <- c(0.1, 0.4, 0.6)
+sum(repro)
 survive <- c(0.5, 0.7, 0.7)
 comp.mat <- matrix(0.5, nrow = 3, ncol = 3)
 names <- list("Ipomopsis", "Cheilanthes", "Papaver")
 
 info <- setup.plants(repro, survive, comp.mat, names)
-info$comp.mat
+print(info)
+
+
+# test <- c(0.1, 0.4, 0.6, -.3)
+# if(any(test > 1.0) | (any(test < 0))){
+#   print("messed up")
+# }else{
+#   print("ok mate")
+# }
